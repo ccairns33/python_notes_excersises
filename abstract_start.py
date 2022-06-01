b9919e6c9672890a3dbe7f2bb2120da3fc1a97dd
ccairns <67658785+ccairns33@users.noreply.github.com>
2022-06-01 11:47:49 -0500

    add notes/code from exercises

---
+++ abstract_start.py
@@ -0,0 +1,38 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Using Abstract Base Classes to enforce class constraints
+
+from abc import ABC, abstractmethod
+# ABC is abstract base class
+
+
+class GraphicShape(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    # tells python no implementation of the base class and each sub class has to override this method
+    def calcArea(self):
+        pass
+
+
+class Circle(GraphicShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
+
+
+class Square(GraphicShape):
+    def __init__(self, side):
+        self.side = side
+
+    def calcArea(self):
+        return self.side ** 2
+
+# g = GraphicShape()
+
+c = Circle(10)
+print(c.calcArea())
+s = Square(12)
+print(s.calcArea())
---
+++ class_start.py
@@ -0,0 +1,52 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Using class-level and static methods
+
+
+class Book:
+    # TODO: Properties defined at the class level are shared by all instances
+
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
+
+
+    # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
+    # TODO: create a class method
+    @classmethod
+    def get_booktypes(cls):
+        return cls.BOOK_TYPES
+
+    # TODO: create a static method
+    @staticmethod
+    def get_booklist():
+        if Book.__booklist != None:
+            return Book.__booklist
+        else:
+            Book.__booklist = []
+            return Book.__booklist
+
+
+
+    # instance methods receive a specific object instance as an argument
+    # and operate on data specific to that object instance
+    def setTitle(self, newtitle):
+        self.title = newtitle
+
+    def __init__(self, title, booktype):
+        self.title = title
+        if not booktype in Book.BOOK_TYPES:
+            raise ValueError(f"{booktype} is a valid book type")
+        else:
+            self.booktype = booktype
+
+
+# TODO: access the class attribute
+print(Book.get_booktypes())
+
+# TODO: Create some book instances
+b1 = Book("Title 1", "HARDCOVER")
+b2 = Book("Title 2", "PAPERBACK")
+
+# TODO: Use the static method to access a singleton object
+books = Book.get_booklist()
+books.append(b1.title)
+print(books)
